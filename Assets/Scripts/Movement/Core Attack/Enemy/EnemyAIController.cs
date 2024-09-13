b53fb9a4cff02891518f0ebafb284050f61b2acd
Farhan Arief Ramadhan <113919723+FarhanAR09@users.noreply.github.com>
2024-09-13 23:03:16 +0700

    Progress enemy ai state machine

--- Assets/Scripts/Movement/Core Attack/Enemy/EnemyAIController.cs
+++ Assets/Scripts/Movement/Core Attack/Enemy/EnemyAIController.cs
@@ -1,11 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace CoreAttack
 {
+    [RequireComponent(typeof(CAMovementController))]
     public class EnemyAIController : MonoBehaviour
     {
+        //Movement
+        [SerializeField]
+        private SOStateMachine movementStateMachine;
+        private CoreAttack.EnemyMovementState enemyMovementState;
+        private CAMovementController movementController;
+        private bool isMoving = false;
+        private Vector2 targetPosition;
 
+        //Behavior
+        [SerializeField]
+        private SOStateMachine behaviorStateMachine;
+        [SerializeField]
+        private CoreAttack.EnemyBehaviorState enemyBehaviorState;
+
+        private void Awake()
+        {
+            TryGetComponent(out movementController);
+        }
+
+        private void FixedUpdate()
+        {
+            //Movement
+            if (isMoving && movementController != null)
+            {
+                movementController.MoveTo(targetPosition);
+            }
+        }
+
+        public void SetMovementEnable(bool on)
+        {
+            isMoving = on;
+        }
+
+        public void SetMovementTarget(Vector2 targetPosition)
+        {
+            this.targetPosition = targetPosition;
+        }
     }
 }
