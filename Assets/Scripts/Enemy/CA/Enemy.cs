325ad76a2b13a9fc21df2423c18e7056b4fe7f0a
Farhan Arief Ramadhan <113919723+FarhanAR09@users.noreply.github.com>
2024-09-21 22:08:05 +0700

    Added Health

--- Assets/Scripts/Enemy/CA/Enemy.cs
+++ Assets/Scripts/Enemy/CA/Enemy.cs
@@ -6,16 +6,24 @@ namespace CoreAttack
 {
     public class Enemy : MonoBehaviour, ICAHittable, ICAHurtable
     {
+        //Hittable
         public GameObject Owner => gameObject;
+
+        [SerializeField]
+        private Health health;
 
         public void Hit()
         {
-            
+            //Nothing
         }
 
         public void Hurt(float baseDamage)
         {
-            
+            health.TakeDamage(baseDamage);
+            if (health.CurrentHealth <= 0)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
---
+++ Assets/Scripts/Health/CA/Health.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace CoreAttack
+{
+    [Serializable]
+    public class Health
+    {
+        [SerializeField]
+        private float _currentHealth;
+        [SerializeField]
+        private float _maxHealth;
+
+        public UnityAction<float> OnHealthUpdated { get; set; }
+
+        public Health(float maxHealth, float currentHealth = -1)
+        {
+            _maxHealth = maxHealth;
+            if (currentHealth < 0 || currentHealth > maxHealth)
+                _currentHealth = maxHealth;
+            else
+                _currentHealth = currentHealth;
+        }
+
+        public float CurrentHealth => _currentHealth;
+
+        public void TakeDamage(float damage)
+        {
+            _currentHealth = Mathf.Max(0, _currentHealth - damage);
+            OnHealthUpdated?.Invoke(_currentHealth);
+        }
+
+        public void Heal(float amount)
+        {
+            _currentHealth = Mathf.Min(_maxHealth, _currentHealth + amount);
+            OnHealthUpdated?.Invoke(_currentHealth);
+        }
+    }
+}
